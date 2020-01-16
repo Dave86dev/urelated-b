25dc93fcbf6bcd3b96497aca398e9e10d8fe4c6e
Dave86 <davidochando1986@gmail.com>
2020-01-17 00:45:15 +0100

    Estableciendo modelos base de datos.

---
+++ app/oferta.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class oferta extends Model
+{
+    //
+
+
+    protected $primaryKey = 'oferta_id';
+}
---
+++ app/usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class usuario extends Model
+{
+    //
+
+    protected $primaryKey = 'usuario_id';
+}
