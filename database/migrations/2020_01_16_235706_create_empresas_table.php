07d270c8d43ffb3f0e6ebe1913b8703c91aa0f09
Dave86 <davidochando1986@gmail.com>
2020-01-17 19:49:26 +0100

    Campos nullables en varias tablas. Formatear especificamente las tablas iniciado.

--- database/migrations/2020_01_16_235706_create_empresas_table.php
+++ database/migrations/2020_01_16_235706_create_empresas_table.php
@@ -16,15 +16,15 @@ class CreateEmpresasTable extends Migration
         Schema::create('empresas', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
-            $table->string('email');         //email
-            $table->string('password');      //password
-            $table->string('name_reg');      //nombre registrante
-            $table->string('surname_reg');   //apellido registrante
-            $table->string('name');          //nombre empresa
-            $table->string('phone');         //teléfono empresa
-            $table->string('fiscal');        //id fiscal
-            $table->string('description');   //descripción empresa
-            $table->string('sector');        //descripción empresa
+            $table->string('email');                        //email
+            $table->string('password');                     //password
+            $table->string('name_reg');                     //nombre registrante
+            $table->string('surname_reg')->nullable();      //apellido registrante
+            $table->string('name');                         //nombre empresa
+            $table->string('phone')->nullable();            //teléfono empresa
+            $table->string('fiscal')->nullable();           //id fiscal
+            $table->string('description')->nullable();      //descripción empresa
+            $table->string('sector')->nullable();           //descripción empresa
         });
     }
 
