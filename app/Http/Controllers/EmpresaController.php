2a2d0d7db215d0965e913a465ba64c4c4aa09887
rubeneitor <56588038+rubeneitor@users.noreply.github.com>
2020-01-27 15:53:04 +0100

    Endpoint Login Empresa

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -13,7 +13,26 @@ class EmpresaController extends Controller
     }
 
     public function getLoginE($param1, $param2){
-        return Empresa::where('email', 'LIKE', $param1)
-        ->where('password', 'LIKE', $param2)->get();
+        
+        //encontramos a la empresa en concreto
+        $q = Empresa::where('email', 'LIKE', $param1)
+        ->where('password', 'LIKE', $param2)->first()->id;
+
+        //si existe, generamos el token
+        if($q != null){
+            $length = 50;
+            $token = bin2hex(random_bytes($length));
+
+            //guardamos el token en su campo correspondiente
+            Usuario::where('id', '=', $q)
+            ->update(['token' => $token]);
+
+            //devolvemos al front la info necesaria ya actualizada
+            return Usuario::where('email', 'LIKE', $param1)
+            ->where('password', 'LIKE', $param2)->get();
+        }
+        return;
     }
+
+
 }
